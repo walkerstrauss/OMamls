ae080a52243ac835c35ec03cb1f0da4e0a707425
Walker <was236@cornell.edu>
2023-10-15 18:17:54 -0400

    Finished implementing ability.ml

--- src/ability.ml
+++ src/ability.ml
@@ -32,14 +32,73 @@ let sleep_through_class = {
   level = char.level})
 }
 
-let empty_inventory : ability = failwith "Unimplemented"
+let empty_inventory : ability = {
+  name = "Empty inventory";
+  effect = (fun char -> {
+    name = char.name;
+    health = char.health;
+    major = char.major;
+    battle_power = char.battle_power;
+    skills = char.skills;
+    abilities = char.abilities; 
+    inventory = [];
+    experience = char.experience;
+    level = char.level
+  })
+}
+   
 
 let add_item (char : character) (i : item) : character =
-   failwith "Unimplemented"
+   {
+    name = char.name;
+    health = char.health;
+    major = char.major;
+    battle_power = char.battle_power;
+    skills = char.skills; 
+    abilities = char.abilities;
+    inventory = i :: char.inventory;
+    experience = char.experience;
+    level = char.level
+   }
 
 let remove_item (char : character) (i : item) : character = 
-  failwith "Unimplemented "
+  {
+    name = char.name;
+    health = char.health;
+    major = char.major;
+    battle_power = char.battle_power;
+    skills = char.skills;
+    abilities = char.abilities;
+    inventory = List.filter (fun i2 -> (i = i2) = false) char.inventory;
+    experience = char.experience;
+    level = char.level;
+  }
+let take_exam : ability = {
+  name = "Take exam";
+  effect = (fun char -> if char.experience > 500 then {
+    name = char.name;
+    health = char.health;
+    major = char.major;
+    battle_power = char.battle_power;
+    skills = char.skills;
+    abilities = char.abilities;
+    inventory = char.inventory;
+    experience = 0;
+    level = char.level + 1;
+  } else char)
+}
+let study_for_exam : ability = {
+  name = "Study for exam";
+  effect = (fun char -> {
+    name = char.name;
+    health = char.health;
+    major = char.major;
+    battle_power = char.battle_power;
+    skills = char.skills;
+    abilities = char.abilities;
+    inventory = char.inventory;
+    experience = char.experience + 100;
+    level = char.level;
+  })
+}
 
-let study_for_exam : ability = failwith "Unimplemented"
-
-let take_exam : ability = failwith "Unimplemented"
