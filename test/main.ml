16145fc1c7c36df5465880e5ce8e19011cc6bb73
Talia Rubeo <tor5@cornell.edu>
2023-10-18 05:16:31 -0400

    Added some tests for character functions

--- test/main.ml
+++ test/main.ml
@@ -1,3 +1,20 @@
 open OUnit2
-let tests = "Omamls test suite" >::: [ ]
+open Omamls
+open Character
+open Item
+
+(********************************************************************
+   Character Tests
+ ********************************************************************)
+
+let char1 = Character.create "Talia" CS
+
+let tests = "Omamls test suite" >::: [
+  ("Character name change">::fun _ -> (assert_equal "Momo" (Character.rename "Momo" char1).name));
+  ("Character major change">::fun _ -> (assert_equal ECE (Character.change_maj ECE char1).major));
+  ("Character change hp - still alive">::fun _ -> (assert_equal (10,100) (Character.change_hp (-90) char1).health));
+  ("Character change hp - still alive">::fun _ -> (assert_equal Alive (Character.change_hp (-90) char1).status));
+  ("Character change hp - dead">::fun _ -> (assert_equal ((-20),100) (Character.change_hp (-120) char1).health));
+  ("Character change hp - dead">::fun _ -> (assert_equal Dead (Character.change_hp (-120) char1).status));
+]
 let _ = run_test_tt_main tests
