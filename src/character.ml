045dc4e8af299e087af6d4a21aee5f0b1f606758
Gregor Guerrier <gg429@cornell.euu>
2023-10-18 15:27:00 -0400

    Added functions and a status type to the character

--- src/character.ml
+++ src/character.ml
@@ -2,6 +2,7 @@ open Item
 open Ability
 
 type major = CS | ECE | MechE | ChemE | CivilE | IS
+type status = Alive | Dead
 
 type character = {
   name : string;
@@ -13,22 +14,39 @@ type character = {
   inventory : item list;
   experience : int;
   level : int;
+  status : status;
 }
 
-let create char_name char_major = {
-  name = char_name; 
+(**Create the character with a given name and major*)
+let create name major = {
+  name = name; 
   health = 100;
-  major = char_major; 
+  major = major; 
   battle_power = 0; 
   skills = []; 
   abilities = []; 
   inventory = []; 
   experience = 0; 
-  level = 0
+  level = 0;
+  status = Alive;
   }
 
-let rename char_name char_to_change = {char_to_change with name = char_name}
+(**Rename the character *)
+let rename name character = {character with name = name}
 
-let change_hp change char_to_change = {char_to_change with health = char_to_change.health + change}
+(**Change the health of the character and the status of being dead or alive*)
+let change_hp hp character =  {character with health = character.health + hp; status = if hp > 0 then Alive else Dead}
+
+(**Change the major of the character*)
+let change_maj major character = {character with major = major}
+
+(**Add an item to the character's inventory*)
+let add_item item character = {character with inventory = item :: character.inventory}
+
+(**Remove an item from the character's inventory*)
+let remove_item item character = 
+  match List.find_opt (fun x -> x = item) character.inventory with
+  | Some x -> (Some x, {character with inventory = List.find_all (fun x -> x <> x) character.inventory})
+  | None -> (None, {character with inventory = character.inventory})
 
 
