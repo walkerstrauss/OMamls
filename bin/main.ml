1859972ef0bb19ce643bc65320610b038468a8dc
Walker <was236@cornell.edu>
2023-10-18 21:33:35 -0400

    Finished main for now and added some first and last names to use in generate.

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,45 @@
 open Omamls
 open Character
 
+let first_names = 
+  [
+    "Walker";
+    "Talia";
+    "Momo";
+    "Greg";
+    "Cyrus";
+    "Billy";
+    "Lily";
+    "Melissa";
+    "Chris";
+    "John";
+  ]
+
+  let last_names = 
+  [
+    "Strauss";
+    "Rubeo";
+    "Shummo";
+    "Guerrier";
+    "Irani";
+    "Simonelli";
+    "Winagle";
+    "Eckert";
+    "Stewart";
+    "Smith"
+  ]
 (********** command line interface **********)
 let () = 
   print_endline "\n\nWelcome to the Cornell RPG.\n";
   print_endline "Please enter your character name:";
   print_string "> ";
   let name = read_line () in 
-  print_endline ("Your character name will be " ^ name ^ ".");
-  print_endline "Please enter your character's major: \n\ 
-                \"CS\", \"ECE\", \"MechE\", \"ChemE\", \"CivilE\", or \"IS\". \
+  print_endline ("\nYour character name will be " ^ name ^ ".");
+  print_endline "\nPlease enter your character's major: \
+                \n\"CS\", \"ECE\", \"MechE\", \"ChemE\", \"CivilE\", or \"IS\". \
                 Default major is CS!";
+  print_string "> ";
   let major = read_line () in 
-  print_endline ("Your character major will be " ^ major ^ ".");
   let major1 = match major with 
   | "CS" -> CS
   | "ECE" -> ECE
@@ -21,8 +48,16 @@ let () =
   | "CivilE" -> CivilE
   | "IS" -> IS 
   | _ -> CS in 
-  let charac = create name major1 in 
-  print_endline ("Character named " ^ charac.name ^ " majoring in " ^ major ^ 
+  let charac = create name major1 in
+  let major2 = match major1 with 
+  | CS -> "CS"
+  | ECE -> "ECE"
+  | MechE -> "MechE"
+  | ChemE -> "ChemE"
+  | CivilE -> "CivilE"
+  | IS -> "IS" in 
+  print_endline ("Your character major will be " ^ major2 ^ ".");
+  print_endline ("\nCharacter named " ^ charac.name ^ " majoring in " ^ major ^ 
   " has been created.");
-  print_endline "Your character is ready for battle.";
+  print_endline "\nYour character is ready for battle.";
 
