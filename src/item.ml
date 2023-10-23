cbf41a89a00c61c5b303627a5b42af27cd4c2f33
Talia Rubeo <tor5@cornell.edu>
2023-10-23 18:22:12 -0400

    Added items into each catelog in item.ml

--- src/item.ml
+++ src/item.ml
@@ -8,7 +8,7 @@ type item = {
 
 let consumables_catelog = [
     {
-        name = "Ice Tea";
+        name = "Iced Tea";
         description = "Ice Cold Delicious Beverage. Heals 20 health.";
         category = Consumable (20, 4);
     }
@@ -18,15 +18,54 @@ let consumables_catelog = [
         description = "Sweet and savory. Heals 10 health.";
         category = Consumable (10, 4);
     }
+    ;
+    {
+        name = "Cornell Dairy Ice Cream";
+        description = "Legally speaking, this is butter. Heals 25 health.";
+        category = Consumable (25, 5)
+    }
 ]
 
 let supplies_catelog = [
+    {
+        name = "Cornell ID";
+        description = "Your very own ID card! Keep it with you at all times.";
+        category = Supplies ("placeholder", None)
+    }
+    ;
+    {
+        name = "Canada Goose Jacket";
+        description = "Unnecessarily expensive jacket to withstand the cold. Made from real geese!";
+        category = Supplies ("placeholder", None)
+    }
+    ;
+    {
+        name = "Adderall XR 20mg";
+        description = "A very rare commodity due to the National shortage!";
+        category = Supplies ("placeholder", None)
+    }
 ]
 
 let tech_catelog = [
+    {
+        name = "iPad with Apple Pencil";
+        description = "Because you're too good for paper.";
+        category = Tech ("placeholder", None)
+    }
+    ;
+    {
+        name = "MacBook Pro";
+        description = "Expensive laptop.";
+        category = Tech ("placeholder", None)
+    }
 ]
 
 let misc_catelog = [
+    {
+        name = "Poster";
+        description = "From the Cornell Store's poster sale!";
+        category = Tech ("placeholder", None)
+    }
 ]
 
 let items_catelog = [consumables_catelog; supplies_catelog; tech_catelog; misc_catelog]
