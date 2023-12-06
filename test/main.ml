31e86adfb8a70a96dec398ce7b6baa742fdec90c
Walker <was236@cornell.edu>
2023-12-06 17:46:06 -0500

    Six new tests for abilities.

--- test/main.ml
+++ test/main.ml
@@ -1,6 +1,7 @@
 open OUnit2
 open Omamls
 open Character
+open Ability
 
 (********************************************************************
    Character Tests
@@ -33,6 +34,31 @@ let character_tests =
     Ability Tests
   ********************************************************************)
 
+let a1 = Ability.create_ability "Punch" [ ("Stregnth", 1) ] (None, None)
+
+let a2 =
+  Ability.create_ability ""
+    [ ("", 2) ]
+    ( Some { description = "has been punched!"; effect = [ (Damage 20, 1) ] },
+      None )
+
+let ability_tests =
+  [
+    ( "Create ability with no effects name" >:: fun _ ->
+      assert_equal "Punch" a1.name );
+    ( "Create ability with no effects required" >:: fun _ ->
+      assert_equal [ ("Stregnth", 1) ] a1.required );
+    ( "Create ability with no effects effects" >:: fun _ ->
+      assert_equal (None, None) a1.effect );
+    ("Create ability with effects name" >:: fun _ -> assert_equal "" a2.name);
+    ( "Create ability with effects required" >:: fun _ ->
+      assert_equal [ ("", 2) ] a2.required );
+    ( "Create ability with effects effects" >:: fun _ ->
+      assert_equal
+        ( Some { description = "has been punched!"; effect = [ (Damage 20, 1) ] },
+          None )
+        a2.effect );
+  ]
 (********************************************************************
    Item Tests
  ********************************************************************)
@@ -41,8 +67,12 @@ let character_tests =
    Battle Tests
  ********************************************************************)
 
-let one_ability_char = Character.add_ability (List.hd Ability.abilities) (Character.create "" ECE 4 100 0)
-let empty_ability_char = (Character.create "" ECE 4 100 0)
+let one_ability_char =
+  Character.add_ability
+    (List.hd Ability.abilities)
+    (Character.create "" ECE 4 100 0)
+
+let empty_ability_char = Character.create "" ECE 4 100 0
 
 let ability_test_one_ability =
   "Please select the following options (1 - 4):\n\
@@ -61,8 +91,8 @@ let ability_test_no_abilities =
 let battle_tests =
   [
     ( "Character print abilities" >:: fun _ ->
-      assert_equal ability_test_one_ability (Battle.print_abilities one_ability_char)
-        ~printer:(fun x -> x) );
+      assert_equal ability_test_one_ability
+        (Battle.print_abilities one_ability_char) ~printer:(fun x -> x) );
     ( "Print abilities for no abilities" >:: fun _ ->
       assert_equal ability_test_no_abilities
         (Battle.print_abilities empty_ability_char) ~printer:(fun x -> x) );
@@ -70,6 +100,6 @@ let battle_tests =
 
 let test =
   "Test Suite for OMamls: Cornell RPG"
-  >::: List.flatten [ character_tests; battle_tests ]
+  >::: List.flatten [ character_tests; battle_tests; ability_tests ]
 
 let _ = run_test_tt_main test
