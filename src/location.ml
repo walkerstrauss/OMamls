1fcdd223dafe0ac45312c8b4a2cc54b3de111db8
Mohammed Shummo <ms3334@cornell.edu>
2023-12-07 14:37:03 -0500

    Fixed Location bug.

--- src/location.ml
+++ src/location.ml
@@ -1,13 +1,12 @@
 
-type location = 
-  | Store  
-  | Eatery 
-  | Dorm of room
-  | Hall of room
-
 type room = 
   | Home 
   | Lecture 
   | Lounge 
   | Lobby
-  
+
+type location = 
+  | Store  
+  | Eatery 
+  | Dorm of room
+  | Hall of room
--- src/location.mli
+++ src/location.mli
@@ -1,9 +1,3 @@
-(** The type of location that a player is currently located. *)
-type location = 
-  | Store  
-  | Eatery 
-  | Dorm of room
-  | Hall of room
 
 (** Type of Room within a location that a player 
     is currently located. *)
@@ -12,4 +6,10 @@ type room =
   | Lecture 
   | Lounge 
   | Lobby
-  
+
+(** The type of location that a player is currently located. *)
+type location = 
+  | Store  
+  | Eatery 
+  | Dorm of room
+  | Hall of room
